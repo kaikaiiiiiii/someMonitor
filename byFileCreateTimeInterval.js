1a5bcc602d38e6fb97cb2508058cf617e345e9b1
kaikaiiiiiii <yourkaikai@gmail.com>
2023-03-03 17:56:54 +0800

    partially tested

--- byFileCreateTimeInterval.js
+++ byFileCreateTimeInterval.js
@@ -5,25 +5,22 @@ const config = require('./config');
 
 console.log(config);
 
-
-let lastFileCreatedTime = null;
-
 function sendEmail() {
     const transporter = nodemailer.createTransport({
         host: "smtp.qq.com",
         port: 587,
         secure: false,
         auth: {
-            user: EMAIL_ACCOUNT,
-            pass: EMAIL_PASSWORD,
+            user: config.EMAIL_ACCOUNT,
+            pass: config.EMAIL_PASSWORD,
         },
     });
 
     const mailOptions = {
-        from: EMAIL_ACCOUNT,
-        to: EMAIL_RECIPIENT,
-        subject: 'title',
-        text: 'content',
+        from: config.EMAIL_ACCOUNT,
+        to: config.EMAIL_RECIPIENT,
+        subject: config.EMAIL_TITLE,
+        text: config.EMAIL_CONTENT,
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
@@ -35,69 +32,61 @@ function sendEmail() {
     });
 }
 
-function traverseDirectory(dirPath) {
-    const files = fs.readdirSync(dirPath, { withFileTypes: true });
+function traverseDirectory(dirPath, fileType) {
+    let latestDate = null;
 
-    files.forEach((file) => {
-        const filePath = path.join(dirPath, file.name);
-        if (file.isDirectory() && !fs.lstatSync(filePath).isSymbolicLink()) {
-            traverseDirectory(filePath);
-        } else {
-            const fileModifiedTime = fs.statSync(filePath).mtime.getTime();
-            if (!lastFileCreatedTime || fileModifiedTime > lastFileCreatedTime) {
-                lastFileCreatedTime = fileModifiedTime;
+    function traverse(dirPath) {
+        const files = fs.readdirSync(dirPath, { withFileTypes: true });
+
+        for (const file of files) {
+            const filePath = path.join(dirPath, file.name);
+
+            if (file.isSymbolicLink()) { continue }; // 跳过符号链接目录
+
+            if (file.isDirectory()) {
+                traverse(filePath); // 递归遍历子目录
+            } else if (!fileType || path.extname(filePath) === fileType) {
+                const stats = fs.statSync(filePath);
+                const modifiedDate = stats.mtime;
+
+                if (!latestDate || modifiedDate > latestDate) {
+                    latestDate = modifiedDate;
+                }
             }
         }
-    });
-}
-
-function saveHistory() {
-    const now = new Date();
-    const history = `${now.getFullYear()}-${(now.getMonth() + 1)
-        .toString()
-        .padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')} ${now
-            .getHours()
-            .toString()
-            .padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now
-                .getSeconds()
-                .toString()
-                .padStart(2, '0')}.${now.getMilliseconds().toString().padStart(3, '0')}`;
-
-    fs.writeFileSync('history.txt', history);
-}
-
-function readHistory() {
-    try {
-        const history = fs.readFileSync('history.txt', 'utf-8').trim();
-        return new Date(history);
-    } catch (err) {
-        return null;
-    }
-}
-
-function startPolling() {
-    traverseDirectory(DIRECTORY_TO_WATCH);
-
-    let shouldSendEmail = false;
-
-    if (!lastFileCreatedTime) {
-        lastFileCreatedTime = new Date().getTime();
     }
 
-    const lastRunTime = readHistory() || new Date();
-    const elapsedTime = new Date().getTime() - lastRunTime.getTime();
-
-    if (elapsedTime > ALERT_THRESHOLD) {
-        shouldSendEmail = true;
-    }
-
-    saveHistory();
-
-    if (shouldSendEmail) {
-        //sendEmail();
-    }
-
-    setTimeout(startPolling, ALERT_THRESHOLD);
+    traverse(dirPath);
+    return latestDate;
 }
 
-startPolling();
+
+let lastResult;
+let lastTimestamp;
+let warningShown = false;
+
+function check() {
+    const result = traverseDirectory(__dirname, '.js');
+    const timestamp = new Date().getTime();
+    var diff = timestamp - result;
+    var h = Math.floor(diff / 1000 / 60 / 60);
+    var m = Math.floor(diff / 1000 / 60 % 60);
+    var s = Math.floor(diff % 60000) / 1000;
+    console.log(h + ":" + m + ":" + s);
+    if (result === lastResult && lastTimestamp && timestamp - lastTimestamp > config.ALERT_THRESHOLD && !warningShown) {
+        sendEmail();
+        warningShown = true;
+    }
+
+    if (result !== lastResult) {
+        warningShown = false;
+    }
+
+    lastResult = result;
+    lastTimestamp = timestamp;
+
+}
+
+check();
+
+setInterval(check, config.INTERVAL_TIME);
--- config.sample.js
+++ config.sample.js
@@ -5,6 +5,9 @@ const config = {
     EMAIL_ACCOUNT: 'gslbexwy@qq.com', // 发件人邮箱
     EMAIL_PASSWORD: 'tehkcadh', // 发件人邮箱密码
     EMAIL_RECIPIENT: ['kvkwsumy@qq.com'], // 收件人邮箱
+    EMAIL_TITLE: 'title', // 邮件标题
+    EMAIL_CONTENT: 'content', // 邮件内容
+    INTERVAL_TIME: 5 * 1000, // 1分钟
 }
 
 
