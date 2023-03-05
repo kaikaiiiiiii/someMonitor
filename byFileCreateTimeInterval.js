ba20d3fab93b86b5771546ed07a2d8d8aedc3525
kaikaiiiiiii <yourkaikai@gmail.com>
2023-03-05 17:36:19 +0800

    some adjust

--- byFileCreateTimeInterval.js
+++ byFileCreateTimeInterval.js
@@ -24,11 +24,13 @@ function sendEmail() {
         text: config.EMAIL_CONTENT,
     };
 
+    let mailtimestamp = new Date();
+
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log(error);
         } else {
-            console.log('Email sent: ' + info.response);
+            console.log(mailtimestamp + ':Email sent: ' + info.response);
         }
     });
 }
@@ -65,8 +67,8 @@ function traverseDirectory(dirPath, fileType) {
 let lastTraversedTime = traverseDirectory(config.DIRECTORY_TO_WATCH, config.MONITOR_TYPE);
 // 只在程序启动时执行一次，获得当前时间
 let lastChangeTime = new Date().getTime();
-// 初始状态下不发送邮件
-let warningShown = false;
+// 用于避免重复发送邮件，且第一次不发送邮件
+let warningMailFlag = false;
 
 function check() {
     const thisTraversedTime = traverseDirectory(config.DIRECTORY_TO_WATCH, config.MONITOR_TYPE);
@@ -77,17 +79,17 @@ function check() {
     var h = ('0' + Math.floor(diff / 1000 / 60 / 60)).slice(-2);
     var m = ('0' + Math.floor(diff / 1000 / 60 % 60)).slice(-2);
     var s = ('0' + Math.floor(diff % 60000) / 1000).slice(-6);
-    process.stdout.write("Status: " + h + ":" + m + ":" + s + "\r");
-    //console.log("Status: " + h + ":" + m + ":" + s + "\r");
+    // process.stdout.write("Status: " + h + ":" + m + ":" + s + "\r");
+    console.log("Status: " + h + ":" + m + ":" + s + "\r");
 
     if (thisTraversedTime !== lastTraversedTime) { // 最新修改时间有变化，说明文件更新了，重置三项状态
-        warningShown = false;
+        warningMailFlag = true; //默认是 false，只有第一次文件更新后才开始启动邮件标记
         lastChangeTime = thisTraversedTime;
         lastTraversedTime = thisTraversedTime; // 更新最新修改时间
     } else { // 无更新则检查时间和邮件发送状态
-        if (thisCheckingTime - lastChangeTime > config.ALERT_THRESHOLD && !warningShown) {
+        if (thisCheckingTime - lastChangeTime > config.ALERT_THRESHOLD && warningMailFlag) {
             sendEmail();
-            warningShown = true;
+            warningMailFlag = false;
         }
     }
 
--- config.sample.js
+++ config.sample.js
@@ -1,14 +1,13 @@
 const config = {
     DIRECTORY_TO_WATCH: 'test', // 监控的目录
-    ALERT_THRESHOLD: 60 * 60 * 1000, // 1小时
-    INTERVAL_TIME: 60 * 1000, // 1分钟
+    ALERT_THRESHOLD: 60 * 60 * 1000, // 监控视为异常的时间阈值，单位毫秒
+    INTERVAL_TIME: 60 * 1000, // 轮询时间间隔，单位毫秒
+    MONITOR_TYPE: ".js", //  监控的文件类型
     EMAIL_ACCOUNT: 'gslbexwy@qq.com', // 发件人邮箱
     EMAIL_PASSWORD: 'tehkcadh', // 发件人邮箱密码
     EMAIL_RECIPIENT: ['kvkwsumy@qq.com'], // 收件人邮箱
     EMAIL_TITLE: 'title', // 邮件标题
     EMAIL_CONTENT: 'content', // 邮件内容
-    INTERVAL_TIME: 5 * 1000, // 1分钟
-    MONITOR_TYPE: ".js" //  监控的文件类型
 }
 
 
